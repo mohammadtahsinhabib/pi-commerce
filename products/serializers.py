aeae44224f250f57e5e2b053304014aa46e9f24d
mohammad tahsin <mohammadtahsinhabib@gmail.com>
2025-08-30 18:56:36 +0600

    StringRelatedField to show category

--- products/serializers.py
+++ products/serializers.py
@@ -7,9 +7,7 @@ class ProductSerializer(serializers.Serializer):
     name =serializers.CharField()
     unit_price = serializers.DecimalField(max_digits=10,decimal_places=2,source="price")
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
-    category = serializers.PrimaryKeyRelatedField(
-        queryset = Category.objects.all()
-    )
+    category = serializers.StringRelatedField()
     
     
 
