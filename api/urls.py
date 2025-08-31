5e496ed3f9e0d201f3bc00583981a3265fa002f0
mohammad tahsin <mohammadtahsinhabib@gmail.com>
2025-08-31 19:21:13 +0600

    jwt setup

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,21 @@
 from django.urls import path, include
 from products.views import *
+from orders.views import *
 from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
 router.register('products', ProductViewSet, basename='product')
 router.register('categories', CategoryViewSet,basename="categories")
+router.register('carts', CartViewSet,basename="cart")
 
-product_router = routers.NestedDefaultRouter(
-    router, 'products', lookup='product')
+product_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 product_router.register('reviews', ReviewViewSet, basename='product-review')
 
+cart_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+cart_router.register("items",CartItemViewSet,basename="cart-item")
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include(product_router.urls))
+    path('', include(product_router.urls)),
+    path('', include(cart_router.urls))
 ]
--- pi__commerce/settings.py
+++ pi__commerce/settings.py
@@ -128,3 +128,11 @@ INTERNAL_IPS = [
 REST_FRAMEWORK = {
     "COERCE_DECIMAL_TO_STRING": False
 }
+
+DJOSER = {
+    # 'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    # 'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
+    # 'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    # 'SEND_ACTIVATION_EMAIL': True,
+    # 'SERIALIZERS': {},
+}
