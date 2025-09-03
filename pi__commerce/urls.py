65ffba232cb3a925ae278ca0131340d9a99ea318
mohammad tahsin <mohammadtahsinhabib@gmail.com>
2025-09-03 17:18:55 +0600

    redirection added

--- pi__commerce/urls.py
+++ pi__commerce/urls.py
@@ -3,10 +3,10 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
-
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from .views import home
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -26,8 +26,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/v1/", include("api.urls")),
-    path("api/v1/auth/", include("djoser.urls")),
-    path("api/v1/auth/", include("djoser.urls.jwt")),
+    path("",home,name="home"),
     path(
         "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
     ),
